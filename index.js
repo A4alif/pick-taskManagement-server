113ad29f7caead632a7aca825db1d220b6ac42c3
alifhasanshah <alifhasanshah@gmail.com>
2023-12-22 19:38:08 +0600

    ongoign api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
@@ -34,75 +34,77 @@ async function run() {
     // task all api
 
     // get api
-    app.get("/api/v1/tasks", async(req, res) => {
+    app.get("/api/v1/tasks", async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { authorEmail: req.query?.email };
       }
       const cursor = taskCollection.find(query);
       const result = await cursor.toArray();
-      res.send({result});
-    })
+      res.send({ result });
+    });
     // single task
-    app.get("/api/v1/single-task/:id", async(req,res) =>{
+    app.get("/api/v1/single-task/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await taskCollection.findOne(query);
-      res.send({result})
-    })
+      res.send({ result });
+    });
 
     // post api
-    app.post("/api/v1/add-task", async(req, res) => {
+    app.post("/api/v1/add-task", async (req, res) => {
       const task = req.body;
       const result = await taskCollection.insertOne(task);
-      res.send({result});
-    })
+      res.send({ result });
+    });
 
     // put api
-    app.put("/api/v1/update-task/:id", async(req, res) => {
-
+    app.put("/api/v1/update-task/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
-      const options = {upsert: true};
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const task = req.body;
-      
+
       const updateDoc = {
         $set: {
           title: task.title,
           description: task.description,
           dueDate: task.dueDate,
-          priority: task.postTags
-        }
-      }
+          priority: task.postTags,
+        },
+      };
       const result = await taskCollection.updateOne(filter, updateDoc, options);
-      res.send({result});
-    })
+      res.send({ result });
+    });
 
     // delete api
-    app.delete("/api/v1/delete-task/:id", async(req, res) => {
+    app.delete("/api/v1/delete-task/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const result = await taskCollection.deleteOne(query);
-      res.send({result})
-    })
+      res.send({ result });
+    });
 
-      // My task cart Collection
+    // My task cart Collection
 
-      // get api
-      app.get("/api/v1/mytask-cart", async(req, res) => {
-        const cursor = myTaskCartCollection.find();
-        const result = await cursor.toArray();
-        res.send({result});
-      })
+    // get api
+    app.get("/api/v1/mytask-cart", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query?.email };
+      }
+      const cursor = myTaskCartCollection.find(query);
+      const result = await cursor.toArray();
+      res.send({ result });
+    });
 
-      // post api
-      app.post("/api/v1/mytask-cart", async(req, res) => {
-        const myTaskCart = req.body;
-        console.log(myTaskCart);
-        const result = await myTaskCartCollection.insertOne(myTaskCart);
-        res.send({result});
-      })
-
+    // post api
+    app.post("/api/v1/mytask-cart", async (req, res) => {
+      const myTaskCart = req.body;
+      console.log(myTaskCart);
+      const result = await myTaskCartCollection.insertOne(myTaskCart);
+      res.send({ result });
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
